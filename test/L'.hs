f97da38717c6993980b24762e28f5f0762f08296
Danny Gratzer <jozefg@cmu.edu>
2014-12-28 20:38:16 -0500

    Added the tests for the rest of L'.hs's functions

--- test/L'.hs
+++ test/L'.hs
@@ -1,6 +1,8 @@
 module L' (leftFolds) where
 import qualified Data.Fold.Common as C
+import Data.Monoid
 import Data.List
+import qualified Data.Set as S
 import Test.Tasty
 import Test.Tasty.QuickCheck
 
@@ -16,9 +18,36 @@ propCount :: TestTree
 propCount = testProperty "Count Works"
             $ \l -> C.run l C.count == length (l :: [Int])
 
+propMconcat :: TestTree
+propMconcat = testProperty "Mconcat Works"
+              $ \l -> C.run (map Sum l) C.mconcat
+                      == mconcat (map Sum l :: [Sum Int])
+
+propMinimum :: TestTree
+propMinimum = testProperty "Minimum Works"
+          $ \l -> C.run l C.minimum == mminimum (l :: [Int])
+  where mminimum [] = Nothing
+        mminimum xs = Just (minimum xs)
+
+propMaximum :: TestTree
+propMaximum = testProperty "Maximum Works"
+          $ \l -> C.run l C.maximum == mmaximum (l :: [Int])
+  where mmaximum [] = Nothing
+        mmaximum xs = Just (maximum xs)
+
+
 propNub :: TestTree
 propNub = testProperty "Nub Works"
           $ \l -> C.run l C.nub == nub (l :: [Int])
+
+propSlowNub :: TestTree
+propSlowNub = testProperty "SlowNub Works"
+          $ \l -> C.run l C.slowNub == nub (l :: [Int])
+
+propIntoSet :: TestTree
+propIntoSet = testProperty "IntoSet Works"
+          $ \l -> C.run l C.intoSet == S.fromList (l :: [Int])
+
 
 propLast :: TestTree
 propLast = testProperty "Last works"
@@ -30,11 +59,18 @@ propChunk :: TestTree
 propChunk = testProperty "Chunk Works"
             $ \l -> C.run l (C.chunk id) == chunk (l :: [Int])
   where chunk [] = []
-        chunk (x : xs) = takeWhile (== x) xs : chunk (dropWhile (== x) xs)
+        chunk (x : xs) =
+          (x : takeWhile (== x) xs) : chunk (dropWhile (== x) xs)
 
 leftFolds :: TestTree
 leftFolds = testGroup "Left Folds" [ propSum
                                    , propProduct
                                    , propCount
+                                   , propMconcat
+                                   , propMinimum
+                                   , propMaximum
                                    , propNub
+                                   , propSlowNub
+                                   , propIntoSet
+                                   , propChunk
                                    , propLast]
